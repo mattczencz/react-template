e94c3f4df25b397d344680c0be1a372e33b100a7
Matt Czencz <mattczencz24@gmail.com>
2021-11-06 14:18:59 -0600

    Updated nav to have links and default variables in them

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,12 +2,12 @@ import * as S from '../styles/NavBars'
 
 function Nav(props) {
     return (
-        <S.Navbar>
-            <S.NavLogoText>Navigation</S.NavLogoText>
+        <S.Navbar type="dark">
+            <S.NavLogoText to="/">Navigation</S.NavLogoText>
             <S.NavItems>
-                <S.NavItem>Components Overview</S.NavItem>
-                <S.NavItem>About</S.NavItem>
-                <S.NavItem>Support</S.NavItem>
+                <S.NavItem to="#">Components Overview</S.NavItem>
+                <S.NavItem to="#">About</S.NavItem>
+                <S.NavItem to="#">Support</S.NavItem>
             </S.NavItems>
         </S.Navbar>
     )
